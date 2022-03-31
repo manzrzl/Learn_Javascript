272feed3e71b7b2c47bb76d183423c8c83bd02d1
Manoj Rijal <maanojriijal@gmail.com>
2022-03-31 12:40:28 +0545

    Files used for learning javascript.
    Files used for learning javascript.

---
+++ script.js
@@ -0,0 +1,77 @@
+//console.log => This is particularly useful when you want to see if a function is executing, 
+//or if a variable is being passed/assigned properly. It's actually rather valuable for figuring out 
+//just what went wrong with your code.
+
+//console.log('Hey');
+//console.warn('This is a warning message')
+//console.error('fetching failed')
+//console.log(window);
+//console.log(document);
+//console.log(innerHeight)
+//console.log(window.screen)
+//console.log(window.screen.height)
+//console.log(screen.height)
+
+//document.write => The Document.write() method writes 
+//a string of text to a document stream opened by document.open().
+
+//document.write("Show me the code!")
+
+//Difference Between const and var and let =>
+//const => A const variable cannot be reassigned: 
+//const price = 10;
+//console.log(price);
+//price = 100;
+//console.log(price)
+//=>Uncaught TypeError: invalid assignment to const 'price'
+
+//var => Variables defined with let can be redeclared.
+//var myprice = 100
+//console.log(myprice);
+//myprice = 200
+//console.log(myprice)
+//=>Doesn't Give Error
+
+//let => Variables defined with let cannot be redeclared.
+//let x = "Manoj Rijal"
+//let x = 0
+//=>Uncaught SyntaxError: redeclaration of let x
+
+//String Concatenation: string.concat(), + , template literals
+//+
+//const price = 100
+//console.log("A pen costs: "+price)
+//string.concat()
+//var myText = "A pencil costs: ".concat(price)
+//console.log(myText)
+//template literals
+//var myText = `The price of a eraser is: ${price}`
+//console.log(myText)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
